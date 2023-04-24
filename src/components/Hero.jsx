fb7f7dfe3daea08ce48c1afd3f88059bfbb6241f
Peter <peter@Peters-MacBook-Pro.local>
2023-04-24 16:32:25 -0700

    modified github porfolio url

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Taesan-Peter-Kim-Code/AI_Summarizer_Web_App", "_blank")
           }
           className='black_btn'
         >
